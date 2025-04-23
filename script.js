24e42fbbd3dec3d9f26f17b17f95bab62162aeb7
Kalani Rodrigo <161814612+KalRodrigo@users.noreply.github.com>
2025-04-23 15:59:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed= new Typed(".textc", {
+    strings: ["an Undergraduate" , "a QA Engineer" , "a Software Developer", "a Fast Learner"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop:true,
+});
